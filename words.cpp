71795fd0c0725bc8599f58c6ea70183d03bb4397
FutureMilk <vshipman@umich.edu>
2026-01-03 22:01:19 -0500

    added factorial fucntion

--- words.cpp
+++ words.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <cmath>
 
-const int LETTER_COUNT = 26;
+const int ALPHABET_NUM = 26;
 
 using namespace std;
 
@@ -19,17 +19,32 @@ int checkForcheck(string &word) {
     return ctr;
 }
 
+// calculate the factorial of the number w/ recursion
+int factorial(int n) {
+  if (n > 1)
+    return n * factorial(n - 1);
+  else
+    return 1;
+}
+
 
 
 int main() {
-    string testWord = "ayqr?";
+    string testWord = "ayqr?be";
+    int wordLength = testWord.length();
     sort(testWord.begin(), testWord.end());
     cout << testWord;
     int min, max = 0;
-    int checks = checkForcheck(testWord);
-    if (checks > 0) {
-       max = pow(LETTER_COUNT, checks);
-       
+    int qMarks = checkForcheck(testWord);
+
+    if (qMarks > 0) {
+        // 26^q * (n-q)!
+        // 26 letter options for each question mark,
+        // factorial the rest of the letter options, minus the question marks
+        max = pow(ALPHABET_NUM, qMarks) * factorial(wordLength - qMarks);
+    } else {
+        max = factorial(wordLength);
     }
+
     return 0;
 }
